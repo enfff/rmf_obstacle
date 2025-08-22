b95911192c06469f17088e5c36c237434bc93dfa
Enfff <enforcetitan@gmail.com>
2025-08-22 17:58:47 +0200

    Logging use_sim_time for debug reasons
    Signed-off-by: Enfff <enforcetitan@gmail.com>

--- rmf_obstacle_detector_laserscan/src/LaserscanDetector.cpp
+++ rmf_obstacle_detector_laserscan/src/LaserscanDetector.cpp
@@ -185,6 +185,12 @@ LaserscanDetector::LaserscanDetector(const rclcpp::NodeOptions& options)
 : LifecycleNode("laserscan_obstacle_detector", options),
   _calibrated(false)
 {
+  const bool use_sim_time = this->get_parameter("use_sim_time").as_bool();
+  RCLCPP_INFO(
+    this->get_logger(),
+    "use_sim_time parameter is set to %s", use_sim_time ? "true" : "false"
+  );
+
   _range_threshold = this->declare_parameter("range_threshold", 1.0);
   RCLCPP_INFO(
     this->get_logger(),
